9838dccdcfe2efd5e85d5eaa06de5530e4112d94
YeoSungeun <dutjddms@naver.com>
2024-05-23 03:09:41 +0900

    [Feat] NewlyCoinedWord 최근검색어 기능 추가

--- EmotionDiary/NewlyCoinedWordViewController.swift
+++ EmotionDiary/NewlyCoinedWordViewController.swift
@@ -21,8 +21,16 @@ class NewlyCoinedWordViewController: UIViewController {
     @IBOutlet var thirdNewlyWordButton:UIButton!
     @IBOutlet var fourthNewlyWordButton: UIButton!
     
+    
     @IBOutlet var resultImageView: UIImageView!
     @IBOutlet var resultLabel: UILabel!
+    
+    @IBOutlet var recentWordLabel: UILabel!
+    
+    @IBOutlet var recentWordButtons: [UIButton]!
+    
+    @IBOutlet var resetButton: UIButton!
+    
     
     var newWords: [String] = ["쓸쓸비용", "편백족", "만반잘부", "이생망", "스불재", "KIJUL", "~그 잡채", "오뱅알", "중꺽그마", "잼얘"]
     var wordMean: [String] = [
@@ -37,8 +45,24 @@ class NewlyCoinedWordViewController: UIViewController {
         "중요한 건 꺾였는데도 그냥 하는 마음",
         "재미있는 이야기의 줄임말"
     ]
-    
     var indexArray = [0, 1, 2, 3]
+    
+    /*
+    @IBOutlet var newlyWordButtons: [UIButton]!
+
+    var newWordsDictionary = [
+        "쓸쓸비용": "외로움을 달래기 위해 쓰는 비용",
+        "편백족":"끼니등을 편의점에서 해결하면서 옷이나 신발등은 백화점에서 명품을 사는 사람",
+        "만반잘부": "만나서 반갑습니다. 잘 부탁드립니다",
+        "이생망": "이번 생은 망했다",
+        "스불재": "스스로 불러온 재앙",
+        "KIJUL": "기절을 영어로 표현한 단어 \nex) 맛있어서 kijul",
+        "~그 잡채": "‘그 자체’의 언어유희",
+        "오뱅알": "오늘 뱅송(방송) 알찼다",
+        "중꺽그마": "중요한 건 꺾였는데도 그냥 하는 마음",
+        "잼얘": "재미있는 이야기의 줄임말"
+    ]
+     */
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,7 +77,9 @@ class NewlyCoinedWordViewController: UIViewController {
         setNewlyWord(button: secondNewlyWordButton, text: "\(newWords[indexArray[1]])")
         setNewlyWord(button: thirdNewlyWordButton, text: "\(newWords[indexArray[2]])")
         setNewlyWord(button: fourthNewlyWordButton, text: "\(newWords[indexArray[3]])")
- 
+        /*
+        setRandomButtonDict(button: newlyWordButtons)
+         */
         
         resultImageView.image = UIImage.background
         resultImageView.contentMode = .scaleAspectFill
@@ -62,8 +88,13 @@ class NewlyCoinedWordViewController: UIViewController {
         resultLabel.numberOfLines = 0
         resultLabel.font = .systemFont(ofSize: 16, weight: .semibold)
         
+        recentWordLabel.text = "최근 검색어"
+        setRecentWordsButtons()
+        setResetButton()
+        
         
     }
+    
     
     // 랜덤신조어 버튼 UI설정 함수
     func setNewlyWord(button: UIButton, text: String) {
@@ -75,6 +106,7 @@ class NewlyCoinedWordViewController: UIViewController {
         button.layer.borderColor = UIColor.black.cgColor
         button.layer.borderWidth = 2
     }
+ 
     
     // 0~신조어배열의개수-1 중 4개의 숫자 랜덤으로 중복없이 뽑기
     func setRandomIndex() {
@@ -104,28 +136,128 @@ class NewlyCoinedWordViewController: UIViewController {
             resultMeaning = "검색 결과가 없습니다."
         } else {
             resultMeaning = wordMean[resultIndex]
+            
         }
         
         return resultMeaning
     }
+    /*
+    func searchMeaningDict(text: String) -> String {
+        guard let resultText = newWordsDictionary[text] else {
+            let messge = "검색 결과가 없습니다."
+            return messge
+        }
+        return resultText
+    }
+     */
+    
+    // 최근 검색어 배열에 searchTextField.text 추가
+    func addRecentWordArray(text: String) {
+        var recentWords = UserDefaults.standard.array(forKey: "recentWords") as? [String] ?? []
+        
+        if text != "" {
+            if recentWords.count < 3 {
+                recentWords.insert(text, at: 0)
+            } else {
+                recentWords.removeLast()
+                recentWords.insert(text, at: 0)
+            }
+            
+            UserDefaults.standard.set(recentWords, forKey: "recentWords")
+            print("userDefault에 최근 검색어 갱신")
+        }
+    }
+    
+    func setRecentWordsButtons() {
+        let recentWords = UserDefaults.standard.array(forKey: "recentWords") as? [String] ?? []
+        print(recentWords)
+        
+        for button in recentWordButtons {
+            button.setTitle("", for: .normal)
+            button.setTitleColor(.gray, for: .normal)
+            button.layer.style = .none
+            button.tintColor = .lightGray
+            button.isHidden = true
+        }
+        
+        let count = recentWords.count
+        
+        if count != 0 {
+            for index in 0...count-1 {
+                recentWordButtons[index].setTitle(recentWords[index], for: .normal)
+                recentWordButtons[index].isHidden = false
+            }
+        }
+    }
+    
+    func setResetButton() {
+        resetButton.setTitle("모두 지우기", for: .normal)
+        resetButton.setTitleColor(.gray, for: .normal)
+        resetButton.titleLabel?.font = .systemFont(ofSize: 12)
+        resetButton.layer.style = .none
+        resetButton.tintColor = .lightGray
+    }
     
     
     @IBAction func searchButtonClicked(_ sender: UIButton) {
-        let resultText = searchMeaning(text: searchTextField.text ?? "")
+        
+        let searchText = searchTextField.text ?? ""
+        // 최근 검색어 배열에 searchTextField.text 추가
+        addRecentWordArray(text: searchText)
+        setRecentWordsButtons()
+     
+        
+        let resultText = searchMeaning(text: searchText)
+        /*
+        let resultText = searchMeaningDict(text: searchTextField.text ?? "")
+         */
         resultLabel.text = resultText
     }
     
     @IBAction func returnKeyClicked(_ sender: UITextField) {
-        let resultText = searchMeaning(text: searchTextField.text ?? "")
+        let searchText = searchTextField.text ?? ""
+        // 최근 검색어 배열에 searchTextField.text 추가
+        addRecentWordArray(text: searchText)
+        setRecentWordsButtons()
+      
+        
+        let resultText = searchMeaning(text: searchText)
+        /*
+        let resultText = searchMeaningDict(text: searchTextField.text ?? "")
+         */
         resultLabel.text = resultText
+    }
+    
+    @IBAction func recentWordButtonClicked(_ sender: UIButton) {
+        let word = sender.titleLabel?.text ?? ""
+        searchTextField.text = word
+        addRecentWordArray(text: word)
+        setRecentWordsButtons()
+        let resultText = searchMeaning(text: word)
+        resultLabel.text = resultText
+    }
+    
+    
+    @IBAction func resetButtonClicked(_ sender: UIButton) {
+        for key in UserDefaults.standard.dictionaryRepresentation().keys {
+            UserDefaults.standard.removeObject(forKey: key.description)
+        }
+        
+        setRecentWordsButtons()
     }
     
     
     @IBAction func randomWordButtonClicked(_ sender: UIButton) {
         var resultText: String = ""
         
+        
+        
         searchTextField.text = newWords[indexArray[sender.tag]]
         resultText = wordMean[indexArray[sender.tag]]
+        
+        // 최근검색어 추가
+        addRecentWordArray(text: sender.titleLabel?.text ?? "")
+        setRecentWordsButtons()
         
         resultLabel.text = resultText
         setRandomIndex()
@@ -134,6 +266,10 @@ class NewlyCoinedWordViewController: UIViewController {
         secondNewlyWordButton.setTitle(newWords[indexArray[1]], for: .normal)
         thirdNewlyWordButton.setTitle(newWords[indexArray[2]], for: .normal)
         fourthNewlyWordButton.setTitle(newWords[indexArray[3]], for: .normal)
+        /*
+        resultLabel.text = searchMeaning(text: newlyWordButtons[sender.tag].titleLabel?.text ?? "")
+        setRandomButtonDict(button: newlyWordButtons)
+         */
 
     }
     
@@ -141,6 +277,17 @@ class NewlyCoinedWordViewController: UIViewController {
     @IBAction func endEditingGesture(_ sender: UITapGestureRecognizer) {
         view.endEditing(true)
     }
+    /*
+    func setRandomButtonDict(button: [UIButton]) {
+        for (index, word) in newWordsDictionary.enumerated() {
+            button[index].setTitle(word.key, for: .normal)
+            if index == 3 {
+                return
+            }
+        }
+    }
+    */
+     
     
 }
 
